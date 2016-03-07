e23bf99b49e132dd464686337091470d025c10c8
bruce <452113521@qq.com>
2016-03-07 09:25:36 +0800

    做临时保存，后面将会对字符串更新的方式数据结构做调整

---
+++ strtableUpdate/strtableUpdate/没有使用的类/CountryValue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strtableUpdate
+{
+    //后期有许多对索引的操作，用类的话不好处理暂不用
+    /// <summary>
+    /// 国家值，
+    /// </summary>
+    public class CountryValue
+    {
+        List<CountryValue> listCv = new List<CountryValue>();
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<CountryValue> ListCv
+        {
+            get { return listCv; }
+            set { listCv = value; }
+        }
+        string countrye = "";
+
+        public string Countrye
+        {
+            get { return countrye; }
+            set { countrye = value; }
+        }
+        string value = "";
+
+        /// <summary>
+        /// 值
+        /// </summary>
+        public string Value
+        {
+            get { return this.value; }
+            set { this.value = value; }
+        }
+        bool isReplace = false;
+
+        /// <summary>
+        /// 是否替换
+        /// </summary>
+        public bool IsReplace
+        {
+            get { return isReplace; }
+            set { isReplace = value; }
+        }
+    }
+}
---
+++ strtableUpdate/strtableUpdate/没有使用的类/IDCountry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strtableUpdate
+{
+    //后期有许多对索引的操作，用类的话不好处理暂不用
+    class IDCountry
+    {
+        string iD = "";
+        /// <summary>
+        /// 如果有相同的出现，只取第一次跳过第二次
+        /// </summary>
+        public string ID
+        {
+            get { return iD; }
+            set { iD = value; }
+        }
+        CountryValue cv = new CountryValue();
+
+        public CountryValue Cv
+        {
+            get { return cv; }
+            set { cv = value; }
+        }
+
+    }
+}
